3d036adf4d6876cfae45ca6fa21981f1f91d63a2
furyhawk <furyx@hotmail.com>
2025-02-19 14:04:11 +0800

    Refactor Download Offline Map page to use stateful widget and implement caching with Dio

--- lib/pages/download_offline_map.dart
+++ lib/pages/download_offline_map.dart
@@ -1,35 +1,87 @@
 import 'package:flutter/material.dart';
+import 'dart:io';
+import 'package:logger/logger.dart';
+
+import 'package:dio_cache_interceptor/dio_cache_interceptor.dart';
+import 'package:dio_cache_interceptor_file_store/dio_cache_interceptor_file_store.dart';
+
 import 'package:flutter_map/flutter_map.dart';
-import 'package:flutter_offline_map/misc/tile_providers.dart';
+import 'package:flutter_map_cache/flutter_map_cache.dart';
+import 'package:path_provider/path_provider.dart';
+
 import 'package:flutter_offline_map/widgets/drawer/menu_drawer.dart';
 import 'package:latlong2/latlong.dart';
 
-class DownloadOfflineMap extends StatelessWidget {
+class DownloadOfflineMap extends StatefulWidget {
   static const String route = '/download_offline_map';
-
   const DownloadOfflineMap({super.key});
 
   @override
+  State<DownloadOfflineMap> createState() => _DownloadOfflineMapState();
+}
+
+class _DownloadOfflineMapState extends State<DownloadOfflineMap> {
+  // create the cache store as a field variable
+  final Future<CacheStore> _cacheStoreFuture = _getCacheStore();
+  var logger=Logger();
+
+  /// Get the CacheStore as a Future. This method needs to be static so that it
+  /// can be used to initialize a field variable.
+  static Future<CacheStore> _getCacheStore() async {
+    final dir = await getTemporaryDirectory();
+    // Note, that Platform.pathSeparator from dart:io does not work on web,
+    // import it from dart:html instead.
+    return FileCacheStore('${dir.path}${Platform.pathSeparator}MapTiles');
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Download Offline Map')),
-      drawer: const MenuDrawer(route),
-      body: FlutterMap(
-        options: MapOptions(
-          initialCenter: const LatLng(1.29027, 103.851959),
-          minZoom: 12,
-          maxZoom: 17,
-          cameraConstraint: CameraConstraint.containCenter(
-            bounds: LatLngBounds(
-              const LatLng(1.33327, 103.931959),
-              const LatLng(1.21027, 103.801959),
+    return FutureBuilder<CacheStore>(
+      future: _cacheStoreFuture,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          final cacheStore = snapshot.data!;
+          logger.d(Platform.operatingSystem);
+          return Scaffold(
+            appBar: AppBar(title: const Text('Download Offline Map')),
+            drawer: const MenuDrawer(DownloadOfflineMap.route),
+            body: FlutterMap(
+              options: MapOptions(
+                initialCenter: const LatLng(1.29027, 103.851959),
+                minZoom: 12,
+                maxZoom: 17,
+                cameraConstraint: CameraConstraint.containCenter(
+                  bounds: LatLngBounds(
+                    const LatLng(1.33327, 103.931959),
+                    const LatLng(1.21027, 103.801959),
+                  ),
+                ),
+              ),
+              children: [
+                TileLayer(
+                  urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                  tileProvider: CachedTileProvider(
+                    // use the store for your CachedTileProvider instance
+                    store: cacheStore,
+                  ),
+                ),
+              ],
             ),
-          ),
-        ),
-        children: [
-          openStreetMapTileLayer,
-        ],
-      ),
+          );
+        }
+        if (snapshot.hasError) {
+          return Scaffold(
+            appBar: AppBar(title: const Text('Download Offline Map')),
+            drawer: const MenuDrawer(DownloadOfflineMap.route),
+            body: Center(child: Text('Error: ${snapshot.error.toString()}')),
+          );
+        }
+        return Scaffold(
+          appBar: AppBar(title: const Text('Download Offline Map')),
+          drawer: const MenuDrawer(DownloadOfflineMap.route),
+          body: Center(child: CircularProgressIndicator()),
+        );
+      },
     );
   }
 }
